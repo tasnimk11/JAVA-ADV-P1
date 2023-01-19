d70a2c8be0998aed1d2ac6ebba182e08d180edc1
Sofiene29 <99467922+Sofiene29@users.noreply.github.com>
2023-01-19 11:49:54 +0100

    Broadcast Address récupéré automatiquement

---
+++ src/test/java/TestBroadcastAddress.java
@@ -0,0 +1,7 @@
+import fr.projava.triangle.Controllers.NetworkController;
+
+public class TestBroadcastAddress {
+    public static void main(String[] args) {
+        System.out.println("heyy"+NetworkController.findBroadcastAddress());
+    }
+}
