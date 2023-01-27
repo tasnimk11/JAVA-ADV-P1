e4abdbcbaf5fd76defb59d38b874b7b21d86f0ff
tasnim-insa <tasnimk1108@gmail.com>
2023-01-27 13:18:53 +0100

    Final version : jar working on Windows

--- src/main/java/fr/projava/triangle/Controllers/DatabaseController.java
+++ src/main/java/fr/projava/triangle/Controllers/DatabaseController.java
@@ -8,20 +8,64 @@ import java.util.ArrayList;
 
 public class DatabaseController {
     static Connection connection = null;
-    public DatabaseController() {
-        try {
-            String url = "jdbc:sqlite:" + getClass().getClassLoader().getResource("db/triangle.db").getPath();
-            connection = DriverManager.getConnection(url);
-            System.out.println("[DATABASE CONTROLLER] : Connection to database.");
-        } catch (SQLException e) {
-            System.out.println("[DATABASE CONTROLLER] : "+ e.getMessage());
+    public DatabaseController() throws SQLException {
+        String url = "jdbc:sqlite:triangle.db";
+        connection = DriverManager.getConnection(url);
+        System.out.println("[DATABASE CONTROLLER] : Connection to database.");
+
+        if(!connection.isClosed()) {
+            System.out.println("[DATABASE CONTROLLER] : Connection to database is successful.");
+        } else {
+            System.out.println("[DATABASE CONTROLLER] : Connection to database failed.");
         }
+
+        //TODO : Verify if tables exist + create them if not
+        ResultSet usersTable = connection.getMetaData().getTables(null, null, "users", null);
+        if(usersTable.next()) {
+            System.out.println("[DATABASE CONTROLLER] : Table users exists");
+        } else {
+            System.out.println("[DATABASE CONTROLLER] : Table users does not exist");
+            createUsersTable();
+        }
+
+        ResultSet historyTable = connection.getMetaData().getTables(null, null, "chat_history", null);
+        if(historyTable.next()) {
+            System.out.println("[DATABASE CONTROLLER] : Table chat_history exists");
+        } else {
+            System.out.println("[DATABASE CONTROLLER] : Table chat_history does not exist");
+            createHistoryTable();
+        }
+
+    }
+
+    private void createUsersTable() throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.setQueryTimeout(30);  // set timeout to 30 sec.
+        statement.executeUpdate("CREATE Table users( " +
+                "    MAC_address VARCHAR(30) not null unique," +
+                "    Pseudo       VARCHAR(20) not null," +
+                "    User_ID      VARCHAR     not null);"
+        );
+        System.out.println("[DATABASE CONTROLLER] : Table users created");
+    }
+
+    private void createHistoryTable() throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.setQueryTimeout(30);  // set timeout to 30 sec.
+        statement.executeUpdate("CREATE TABLE chat_history(" +
+                "    Remote_User VARCHAR(17)           not null," +
+                "    Message      VARCHAR(100)          not null," +
+                "    Sent_At      DATETIME default CURRENT_TIMESTAMP," +
+                "    Sender       BOOLEAN  default TRUE not null," +
+                "    User_ID      VARCHAR);"
+        );
+        System.out.println("[DATABASE CONTROLLER] : Table chat_history created");
     }
 
     /*
-    * Verifies if MAC isn't already used
-    * which means that : if any account is already defined for this computer
-    * */
+     * Verifies if MAC isn't already used
+     * which means that : if any account is already defined for this computer
+     * */
     public static  String existingMAC(String mac) throws SQLException {
         String reqSQL= "SELECT * " +
                 "FROM users " +
@@ -36,8 +80,8 @@ public class DatabaseController {
     }
 
     /*
-    * Verifies if an account was created using this MAC address and with this pseudo
-    * */
+     * Verifies if an account was created using this MAC address and with this pseudo
+     * */
     public static String existingAccount(String mac, String pseudo) throws SQLException {
         String reqSQL="SELECT * " +
                 "FROM users " +
@@ -45,7 +89,7 @@ public class DatabaseController {
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery(reqSQL);
         if (!rs.next()){
-             return "pseudo_new";
+            return "pseudo_new";
         } else {
             return "pseudo_exists";
         }
@@ -76,8 +120,8 @@ public class DatabaseController {
 
 
     /*
-    * Add a new entry to DB : id, pseudo and mac
-    * */
+     * Add a new entry to DB : id, pseudo and mac
+     * */
     public static void addUser(String uid, String mac, String pseudo) throws SQLException {
         String reqSQL= "INSERT INTO users (User_ID,MAC_address,Pseudo) VALUES ('"+ uid+"',+ '"+ mac+"','" + pseudo + "')";
         Statement statement = connection.createStatement();
@@ -105,8 +149,8 @@ public class DatabaseController {
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery(reqSQL);
         while(rs.next()){
-                //h.add(new Message(rs.getBoolean("Sender"), rs.getString("Message"), rs.getDate("Sent_At")));
-                h.add(new Message(rs.getBoolean("Sender"), rs.getString("Message")));
+            //h.add(new Message(rs.getBoolean("Sender"), rs.getString("Message"), rs.getDate("Sent_At")));
+            h.add(new Message(rs.getBoolean("Sender"), rs.getString("Message")));
         }
         return h;
     }
